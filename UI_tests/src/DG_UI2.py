515cd785b5407970b6ecb9d7284d9cb98f2a6d55
carlos garcia <carlos@carlosgarcia.com>
2016-09-07 16:46:35 -0700

    Added animation to the dock

--- UI_tests/src/DG_UI2.py
+++ UI_tests/src/DG_UI2.py
@@ -25,7 +25,7 @@ class MainDialog(qg.QDialog):
         self.setWindowFlags(qc.Qt.WindowStaysOnTopHint)
         self.setObjectName('InterpolateIt')
         self.setWindowTitle('Interpolate It')
-        self.setFixedWidth(314)
+        self.setFixedWidth(340)
         
         self.setLayout(qg.QVBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
@@ -58,9 +58,34 @@ class MainDialog(qg.QDialog):
         main_layout.addLayout(button_layout)
         
         inter_widget = InterpolateItWidget()
+        inter_widget.hide_close()
         self.inter_layout.addWidget(inter_widget)
+        
+        self.list_widgets = []
+        self.list_widgets.append(inter_widget)
+        
+        new_btn.clicked.connect(self.add_new)
 
+    def add_new(self):
+        new_inter_widget = InterpolateItWidget()
+        self.list_widgets.append(new_inter_widget)
+        self.inter_layout.addWidget(new_inter_widget)
+        self.connect(new_inter_widget, qc.SIGNAL('CLOSE'), self.remove)
+        new_inter_widget.setFixedHeight(0)
+        new_inter_widget._animation_expand(True)
+        
+    def remove(self, new_widget):
+        self.list_widgets.remove(new_widget)
+        self.connect(new_widget, qc.SIGNAL('DELETE'), self.delete)
+        new_widget._animation_expand(False)
+        
+    def delete(self, new_widget):
+        self.inter_layout.removeWidget(new_widget)
+        new_widget._animation = None
+        new_widget.deleteLater()
+        
 
+# ------------------------------- UI ---------------------------------------
 class InterpolateItWidget(qg.QFrame):
     def __init__(self):
         qg.QFrame.__init__(self)
@@ -98,12 +123,12 @@ class InterpolateItWidget(qg.QFrame):
         self.layout().addLayout(buttons_2_layout)
         
         slider_layout = qg.QHBoxLayout()
-        slider_start_lb = qg.QLabel('Start')
-        slider = qg.QSlider(qc.Qt.Horizontal)
-        slider_end_lb = qg.QLabel('End')
-        slider_layout.addWidget(slider_start_lb)
-        slider_layout.addWidget(slider)
-        slider_layout.addWidget(slider_end_lb)
+        self.slider_start_lb = qg.QLabel('Start')
+        self.slider = qg.QSlider(qc.Qt.Horizontal)
+        self.slider_end_lb = qg.QLabel('End')
+        slider_layout.addWidget(self.slider_start_lb)
+        slider_layout.addWidget(self.slider)
+        slider_layout.addWidget(self.slider_end_lb)
         self.layout().addLayout(slider_layout)
         
         check_layout = qg.QHBoxLayout()
@@ -115,12 +140,74 @@ class InterpolateItWidget(qg.QFrame):
         check_layout.addWidget(self.attrs_chk)
         self.layout().addLayout(check_layout)
         
+        self.exit_btn.clicked.connect(self.remove)
+        
+        self.store_btn.clicked.connect(self.enable_btns)
+        
+        self.set_btns(False)
+        
+    def set_btns(self, value):
+        self.reset_btn.setEnabled(value)
+        self.end_btn.setEnabled(value)
+        self.clean_btn.setEnabled(value)
+        self.start_btn.setEnabled(value)
+        self.slider_start_lb.setEnabled(value)
+        self.slider.setEnabled(value)
+        self.slider_end_lb.setEnabled(value)
+        self.trans_chk.setEnabled(value)
+        self.attrs_chk.setEnabled(value)
+        
+    def enable_btns(self):
+        self.set_btns(True)
+        
+    def hide_close(self):
+        self.exit_btn.setVisible(False)
+        
+    def remove(self):
+        self.emit(qc.SIGNAL('CLOSE'), self)
+    
+    def delete_widget(self):
+        self.emit(qc.SIGNAL('DELETE'), self)
+        
+    # ----------------------------- Animation ----------------------------------
+
+    def _animation_expand(self, value):
+        size_animation = qc.QPropertyAnimation(self, 'geometry')
+        
+        geometry = self.geometry()
+        width = geometry.width()
+        x, y, _, _ = geometry.getCoords()
+        
+        size_start = qc.QRect(x, y, width, int(not(value)) * 150)
+        size_end = qc.QRect(x, y, width, int(value) * 150)
+        
+        size_animation.setStartValue(size_start)
+        size_animation.setEndValue(size_end)
+        size_animation.setDuration(300)
+        
+        anim_curve = qc.QEasingCurve()
+        if value:
+            anim_curve.setType(qc.QEasingCurve.InQuad)
+        else:
+            anim_curve.setType(qc.QEasingCurve.OutQuad)
+            
+        size_animation.setEasingCurve(anim_curve)
+        
+        size_animation.valueChanged.connect(self._force_resize)
+        if not value:
+            size_animation.finished.connect(self.delete_widget)
+        
+        self._animation = size_animation
+        size_animation.start(qc.QAbstractAnimation.DeleteWhenStopped)
+        
+    def _force_resize(self, new_height):
+        self.setFixedHeight(new_height.toRect().height())
+
 
 def run():
     app = qg.QApplication(sys.argv)
     
     main_win = MainWindow()
-    
     main_dialog = MainDialog()
     main_dialog.setParent(main_win)
     size = main_dialog.size()

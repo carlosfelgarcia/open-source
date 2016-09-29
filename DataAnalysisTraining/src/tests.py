7528eb0e23d084c841c34e946a6a2c699b1008c6
carlos garcia <carlosgarcia@myemail.com>
2016-09-29 15:28:21 -0700

    New branch to study data analysis

---
+++ DataAnalysisTraining/src/tests.py
@@ -0,0 +1,13 @@
+'''
+Created on Sep 29, 2016
+
+@author: User
+'''
+
+
+import numpy as np
+from pandas import Series, DataFrame
+import pandas as pd
+from numpy.random import randn
+
+print randn(2, 2, 4, 1)

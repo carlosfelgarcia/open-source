3d1e003c145a41a677a1c636f7d2e76d31883acd
carlos garcia <carlosgarcia@myemail.com>
2016-10-04 16:16:57 -0700

    Test to look for tables in html

--- DataAnalysisTraining/src/tests.py
+++ DataAnalysisTraining/src/tests.py
@@ -6,8 +6,11 @@ Created on Sep 29, 2016
 
 
 import numpy as np
-from pandas import Series, DataFrame
+from pandas import Series, DataFrame, read_html
 import pandas as pd
 from numpy.random import randn
 
-print randn(2, 2, 4, 1)
+# url = 'http://www.carlosgarcia.co'
+# # It looks for tables in html
+# url_data =  pd.io.html.read_html(url)
+# print url_data[0]

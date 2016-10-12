4f72bdb441882168b1426f2965fc06469ad97e27
carlos garcia <carlosgarcia@myemail.com>
2016-10-12 11:39:59 -0700

    Main and UI classes added

---
+++ DataAnalysisTraining/titanic_kaggle/UI.py
@@ -0,0 +1,8 @@
+'''
+Created on Oct 12, 2016
+
+@author: User
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ DataAnalysisTraining/titanic_kaggle/main.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 12, 2016
+
+@author: User
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        

ac3de5c6fefac8e48cefe94374431c599e82c473
carlos garcia <carlosgarcia@myemail.com>
2017-03-19 20:51:26 -0700

    All classes are documented

--- DataAnalysisTraining/titanic_kaggle/interface_data.py
+++ DataAnalysisTraining/titanic_kaggle/interface_data.py
@@ -8,14 +8,25 @@ from abc import ABCMeta, abstractmethod
 
 class DataInterface:
     '''
-    classdocs
+    This interface class it force the IO classes to have at least this methods
+    to communicate with the main class
     '''
     __metaclass__ = ABCMeta
-    
+
     @abstractmethod
     def read_file(self, path):
+        '''
+        It reads a file
+        :param file_path: The path where the file is in the system
+        :type file_path: str
+        '''
         pass
-    
+
     @abstractmethod
     def write_file(self, path, content):
+        '''
+        It writes a file
+        :param path: The path where the file is going to be save
+        :type path: str
+        '''
         pass
